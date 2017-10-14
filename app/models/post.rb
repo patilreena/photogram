6f27a47205ccbb57c42418289f10e518e6786034
Reena Patil <reena.patil@outlook.com>
2017-10-14 13:20:30 +1100

    photogram app

---
+++ app/models/post.rb
@@ -0,0 +1,14 @@
+# class Post < ApplicationRecord
+# 	 has_attached_file :image
+# end
+
+
+class Post < ActiveRecord::Base
+	validates :user_id, presence: true
+  validates :image, presence: true
+
+  has_attached_file :image, styles: { :medium => "640x" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  belongs_to :user
+end
---
+++ db/migrate/20171012010201_add_user_name_to_users.rb
@@ -0,0 +1,5 @@
+class AddUserNameToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_index :users, :user_name, unique: true
+  end
+end
